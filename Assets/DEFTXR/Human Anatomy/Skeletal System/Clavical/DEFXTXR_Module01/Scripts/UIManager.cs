e5f26e0f2e6b7c571f06059c724cd8a1758fc355
Ayodhyanandan Bhosale <73810857+SattvariseTechonologiesPvtLtd@users.noreply.github.com>
2023-10-16 10:48:23 +0530

    Commit 6

---
+++ Assets/DEFTXR/Human Anatomy/Skeletal System/Clavical/DEFXTXR_Module01/Scripts/UIManager.cs
@@ -0,0 +1,449 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+   
+    public string selectedBtnName;
+   
+
+    public GameObject zoneAPlat, zoneBplat, zoneCplat;
+    public static UIManager Instance;
+
+    public GameObject femurBone, tibiaBone, patellaBone, hipBone, fibulaBone, sacrumBone;
+    public GameObject clavicleBone, scapulaBone, humerusBone, ulnaRadiusBone, carpalBone;
+    //public GameObject clavicaleBone,scapulaBone,humerusBone,ulnaradiusBone,femurBone,tibiaBone,carpalBone,patellaBone,hipBone;
+    
+    // Sub-Menu reference to on off on click of buttn
+    public GameObject upperLimbMenu, lowerLimbMenu, thoraxMenu, headneckbrainMenu,ComingSoonPanel;
+
+   
+
+   
+
+    //Sprites to be replaced as per selection
+    Sprite btnSelected, btnDselected;
+
+    public GameObject mainUI;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+
+    private void Start()
+    {
+
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+        mainUI.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // ShootLaserFromTargetPosition(rightHand.transform.position, rightHand.transform.forward, laserMaxLength);
+
+        //Call and select the UI as per laser collision
+        //selectUIButton();
+
+    }
+
+    /// <summary>
+    /// Methode will match the button selected by laser
+    /// </summary>
+    /// 
+    public void onUpperLimbButtonClick()
+    {
+        upperLimbMenu.SetActive(true);
+        lowerLimbMenu.SetActive(false);
+        thoraxMenu.SetActive(false);
+        headneckbrainMenu.SetActive(false);
+    }
+
+    public void onLowerLimbButtonClick()
+    {
+        upperLimbMenu.SetActive(false);
+        lowerLimbMenu.SetActive(true);
+        thoraxMenu.SetActive(false);
+        headneckbrainMenu.SetActive(false);
+    }
+
+    public void onThoraxButtonClick()
+    {
+        upperLimbMenu.SetActive(false);
+        lowerLimbMenu.SetActive(false);
+        thoraxMenu.SetActive(true);
+        headneckbrainMenu.SetActive(false);
+    }
+
+    public void onHNBButtonClick()
+    {
+        upperLimbMenu.SetActive(false);
+        lowerLimbMenu.SetActive(false);
+        thoraxMenu.SetActive(false);
+        headneckbrainMenu.SetActive(true);
+
+    }
+
+    //Upper Limb Bones - 
+
+    public void onClavicleButtonClick()
+    {
+        
+        //Upper limb bones
+        clavicleBone.SetActive(true);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        //zoneCplat.SetActive(true);
+
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+    }
+
+    public void onScapulaButtonClick()
+    {
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(true);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        //zoneCplat.SetActive(true);
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+    }
+
+    public void onHumerusButtonClick()
+    {
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(true);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        // zoneCplat.SetActive(true);
+
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+    }
+
+    public void onUlnaRadiusButtonClick()
+    {
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(true);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        // zoneCplat.SetActive(true);
+    }
+
+    public void onCarpalButtonClick()
+    {
+
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(true);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        // zoneCplat.SetActive(true);
+    }
+
+    //Lower Limb Bones - 
+
+    public void onFemurButtonClick()
+    {
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(true);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+
+        //zoneBplat.SetActive(true);
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+
+
+    }
+    
+
+    public void onTibiaButtonClick()
+    {
+
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(true);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        // zoneCplat.SetActive(true);
+    }
+
+    
+
+    public void onPatellaButtonClick()
+    {
+
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(true);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        // zoneCplat.SetActive(true);
+    }
+
+    public void onHipBoneButtonClick()
+    {
+
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(true);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        // zoneCplat.SetActive(true);
+    }
+
+    public void onFibulaButtonClick()
+    {
+
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(true);
+        sacrumBone.SetActive(false);
+
+
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        // zoneCplat.SetActive(true);
+    }
+
+    public void onSacrumButtonClick()
+    {
+
+        //Upper limb bones
+
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(true);
+
+
+        mainUI.SetActive(false);
+        ComingSoonPanel.SetActive(false);
+        //zoneAPlat.SetActive(true);
+        //zoneBplat.SetActive(true);
+        // zoneCplat.SetActive(true);
+    }
+
+    public void onCMPButtonClick()
+    {
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        femurBone.SetActive(false);
+
+        ComingSoonPanel.SetActive(true);
+
+    }
+
+    public void onOkayButtonClick()
+    {
+        ComingSoonPanel.SetActive(false);
+    }
+
+    public void onHomeButtonClick()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
+
+    public void onSkeletonSystemHomeButtonClick()
+    {
+        clavicleBone.SetActive(false);
+        scapulaBone.SetActive(false);
+        humerusBone.SetActive(false);
+        ulnaRadiusBone.SetActive(false);
+        carpalBone.SetActive(false);
+
+        //LowerLimb Bones
+        femurBone.SetActive(false);
+        tibiaBone.SetActive(false);
+        patellaBone.SetActive(false);
+        hipBone.SetActive(false);
+        fibulaBone.SetActive(false);
+        sacrumBone.SetActive(false);
+
+
+        mainUI.SetActive(true);
+    }
+
+
+
+
+}
---
+++ Assets/DEFTXR/Human Anatomy/Skeletal System/Patella/Scripts_Yash/Patella_GameManager.cs
@@ -0,0 +1,242 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Patella_GameManager : MonoBehaviour
+{
+    // public GameObject ZoneA, ZoneB, ZoneC;
+    public GameObject zoneBObj;
+
+    public GameObject insertionObj,  DefaultObj, ligamentObj;
+
+    public GameObject insertionDown, insertionUp, ligamentsDown, ligamentsUp;
+
+    public bool attch, inserAttch, ligamentAttach = false;
+
+    public Sprite enable, disable;
+
+    public GameObject[] insertionsList;
+    public GameObject[] ligamentsList;
+
+    public bool isAllInsertionsSelected;
+    public bool isAllLigamentsSelected;
+
+    public GameObject ligaments_dropdown;
+    public GameObject ligamentsSelectAllButtonTick;
+    public GameObject ligamentsSubButtonsParent;
+
+    public GameObject insertion_dropdown;
+    public GameObject insertionsSelectAllButtonTick;
+    public GameObject insertionsSubButtonsParent;
+
+    public GameObject insertionBtn;
+    public GameObject ligamentsBtn;
+
+    public GameObject ligamentsSelectText;
+    public GameObject ligamentsDeselectText;
+
+    public GameObject insertionSelectText;
+    public GameObject insertionDeselectText;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        isAllInsertionsSelected = false;
+        isAllLigamentsSelected = false;
+
+        insertion_dropdown.SetActive(false);
+        ligaments_dropdown.SetActive(false);
+        
+        
+       // insertionBtn.GetComponent<Image>().sprite = disable;
+       // ligamentsBtn.GetComponent<Image>().sprite = disable;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void selectAllInsertions()
+    {
+        if (isAllInsertionsSelected == false)
+        {
+            insertionsSelectAllButtonTick.SetActive(true);
+
+            for (int i = 0; i < insertionsList.Length; i++)
+            {
+                insertionsList[i].SetActive(true);
+            }
+            foreach (Transform a in insertionsSubButtonsParent.transform)
+            {
+               // a.GetChild(1).transform.GetChild(0).gameObject.SetActive(true);
+                a.Find("TickParent").transform.GetChild(0).gameObject.SetActive(true);
+            }
+            isAllInsertionsSelected = true;
+            insertionDeselectText.SetActive(true);
+            insertionSelectText.SetActive(false);
+        }
+        else
+        {
+            insertionsSelectAllButtonTick.SetActive(false);
+
+            for (int i = 0; i < insertionsList.Length; i++)
+            {
+                insertionsList[i].SetActive(false);
+            }
+
+            foreach (Transform z in insertionsSubButtonsParent.transform)
+            {
+               // z.GetChild(1).transform.GetChild(0).gameObject.SetActive(false);
+                z.Find("TickParent").transform.GetChild(0).gameObject.SetActive(false);
+
+            }
+            isAllInsertionsSelected = false;
+            insertionDeselectText.SetActive(false);
+            insertionSelectText.SetActive(true);
+        }
+    }
+
+    public void selectAllLigaments()
+    {
+        if (isAllLigamentsSelected == false)
+        {
+            ligamentsSelectAllButtonTick.SetActive(true);
+
+            for (int l = 0; l < ligamentsList.Length; l++)
+            {
+                ligamentsList[l].SetActive(true);
+            }
+            foreach (Transform t in ligamentsSubButtonsParent.transform)
+            {
+               // t.GetChild(1).transform.GetChild(0).gameObject.SetActive(true);
+                t.Find("TickParent").transform.GetChild(0).gameObject.SetActive(true);
+            }
+            isAllLigamentsSelected = true;
+            ligamentsDeselectText.SetActive(true);
+            ligamentsSelectText.SetActive(false);
+        }
+        else
+        {
+            ligamentsSelectAllButtonTick.SetActive(false);
+
+            for (int l = 0; l < ligamentsList.Length; l++)
+            {
+                ligamentsList[l].SetActive(false);
+            }
+
+            foreach (Transform t in ligamentsSubButtonsParent.transform)
+            {
+                //t.GetChild(1).transform.GetChild(0).gameObject.SetActive(false);
+                t.Find("TickParent").transform.GetChild(0).gameObject.SetActive(false);
+
+            }
+
+            isAllLigamentsSelected = false;
+            ligamentsDeselectText.SetActive(false);
+            ligamentsSelectText.SetActive(true);
+        }
+    }
+
+    private void insertionsButtonClickReset()
+    {
+        ligamentAttach = true;
+        onLigamentsButtonClick();
+        isAllLigamentsSelected = true;
+        selectAllLigaments();
+    }
+
+    public void onInsertionsButtonClick()
+    {
+        if (inserAttch == false)
+        {
+            insertionsButtonClickReset();
+
+            insertionObj.SetActive(true);
+           
+            DefaultObj.SetActive(false);
+            ligamentObj.SetActive(false);
+            
+            insertion_dropdown.SetActive(true);
+
+            insertionDown.SetActive(false);
+            insertionUp.SetActive(true);
+
+            // insertionBtn.GetComponent<Image>().sprite = enable;
+            inserAttch = true;
+        }
+        else
+        {
+
+            insertionObj.SetActive(false);
+            
+            DefaultObj.SetActive(true);
+            ligamentObj.SetActive(false);
+            
+            insertion_dropdown.SetActive(false);
+
+            insertionDown.SetActive(true);
+            insertionUp.SetActive(false);
+
+            // insertionBtn.GetComponent<Image>().sprite = disable;
+
+            inserAttch = false;
+        }
+    }
+
+    private void ligamentsButtonClickReset()
+    {
+        inserAttch = true;
+        onInsertionsButtonClick();
+        isAllInsertionsSelected = true;
+        selectAllInsertions();
+
+    }
+
+    public void onLigamentsButtonClick()
+    {
+        if (ligamentAttach == false)
+        {
+            ligamentsButtonClickReset();
+
+            insertionObj.SetActive(false);
+            
+            DefaultObj.SetActive(false);
+            ligamentObj.SetActive(true);
+            
+            ligaments_dropdown.SetActive(true);
+
+            ligamentsDown.SetActive(false);
+            ligamentsUp.SetActive(true);
+
+            // ligamentsBtn.GetComponent<Image>().sprite = enable;
+
+            ligamentAttach = true;
+        }
+        else
+        {
+
+
+            insertionObj.SetActive(false);
+          
+            DefaultObj.SetActive(true);
+            ligamentObj.SetActive(false);
+            
+            ligaments_dropdown.SetActive(false);
+
+            ligamentsDown.SetActive(true);
+            ligamentsUp.SetActive(false);
+
+            // ligamentsBtn.GetComponent<Image>().sprite = disable;
+
+
+            ligamentAttach = false;
+        }
+    }
+
+
+
+}
